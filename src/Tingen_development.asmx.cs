62d9d8b96bf92a3a6978454605e6afac352b2aa2
Chris Banwarth <github@aprettycoolprogram.com>
2024-06-03 17:36:30 -0400

    Prototype b240603

--- src/Tingen_development.asmx.cs
+++ src/Tingen_development.asmx.cs
@@ -107,6 +107,8 @@ namespace Tingen_development
              */
             string configFilePath = TingenConfiguration.GetPath("UAT");
 
+            LogEvent.Primeval(AssemblyName, configFilePath);
+
             TingenSession tnSession = TingenSession.Build(configFilePath, sentOptionObject, sentScriptParameter);
 
             /* First good opportunity to create a trace log.
