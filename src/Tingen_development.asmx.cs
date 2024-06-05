c5432c44fbf486bd4120c5ae99c8dc046ec083f8
Chris Banwarth <github@aprettycoolprogram.com>
2024-06-05 12:43:33 -0400

    Prototype b240605

--- src/Tingen_development.asmx.cs
+++ src/Tingen_development.asmx.cs
@@ -79,9 +79,9 @@ namespace Tingen_development
             string configFilePath         = $@"C:\TingenData\{avatarSystemCode}\Config\Tingen.config";
 
             TingenSession tnSession = TingenSession.Build(sentOptionObject, sentScriptParameter, avatarSystemCode, configFilePath);
-
+            LogEvent.Primeval(Asm);
             TingenSession.Initialize(tnSession);
-
+            LogEvent.Primeval(Asm);
             LogEvent.Trace(1, tnSession.TraceLogs, Asm);
 
             switch (tnSession.Config.TingenMode)
