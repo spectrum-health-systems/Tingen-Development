ddc0a9a2dd7f419710c7d1a7bce338bbbb8a5700
Chris Banwarth <github@aprettycoolprogram.com>
2024-06-05 11:07:51 -0400

    Prototype b240605

--- src/Tingen_development.asmx.cs
+++ src/Tingen_development.asmx.cs
@@ -3,9 +3,9 @@
 // https://github.com/APrettyCoolProgram/Tingen_development
 // Copyright (c) A Pretty Cool Program. All rights reserved.
 // Licensed under the Apache 2.0 license.
-// ================================================================ 240604 =====
+// ================================================================ 240605 =====
 
-// u240604.1308
+// u240605.0851
 
 /* PLEASE READ
  * -----------
@@ -51,8 +51,8 @@ namespace Tingen_development
         public string GetVersion() => "VERSION 24.6";
 
         /// <summary>The starting method for Tingen.</summary>
-        /// <param name="sentOptionObject">The OptionObject sent from myAvatar.</param>
-        /// <param name="sentScriptParameter">The Script Parameter sent from myAvatar.</param>
+        /// <param name="sentOptionObject">The OptionObject sent from Avatar.</param>
+        /// <param name="sentScriptParameter">The Script Parameter sent from Avatar.</param>
         /// <remarks>
         ///  <para>
         ///   - Required by Avatar.<br/>
@@ -63,14 +63,20 @@ namespace Tingen_development
         [WebMethod]
         public OptionObject2015 RunScript(OptionObject2015 sentOptionObject, string sentScriptParameter)
         {
-            /* The assembly name, defined here for use in all log files.*/
+            /* We define the assembly name here so we can use it to write log files throughout the class.*/
             string Asm = Assembly.GetExecutingAssembly().GetName().Name;
 
-            /* <!-- For debugging: LogEvent.Primeval(asm); --> */
+            /* Debugging requires writing a Primeval log, since trace logs aren't available until the Tingen Session is initialized.
+             * This will probably be used relatively often during developement, so it's worth keeping around, but it should be
+             * commneted out in production.
+             */
+            //LogEvent.Primeval(asm);
 
-            // This is the only difference.
-            string avatarSystemCode = "UAT";
-            string configFilePath   = $@"C:\TingenData\{avatarSystemCode}\Config\Tingen.config";
+            /* The only difference between the development and production versions of Tingen is the hardcoded Avatar System Code. For
+             * development, the Avatar System Code is "UAT". For production, the Avatar System Code is "LIVE"
+             */
+            const string avatarSystemCode = "UAT";
+            string configFilePath         = $@"C:\TingenData\{avatarSystemCode}\Config\Tingen.config";
 
             TingenSession tnSession = TingenSession.Build(sentOptionObject, sentScriptParameter, avatarSystemCode, configFilePath);
 
@@ -82,13 +88,24 @@ namespace Tingen_development
             {
                 case "disabled":
                     LogEvent.Trace(2, tnSession.TraceLogs, Asm);
+
+                    /* If Tingen is disabled, we should update the service status files so the necessary users are notified. When
+                     * Tingen is re-enabled, the service status files will need to be manually updated using the Admin Module.
+                     */
                     Outpost31.Module.Admin.Service.AllUpdate(tnSession);
-                    Outpost31.Core.Avatar.OptionObjects.CloneSentToReturn(tnSession);
+
+                    /* Normally we would let Outpost31.Core.OptionObject handle this, but since we really want to limit what is done
+                     * while Tingen is disabled, we'll just clone the SentObject to the ReturnObject and return it.
+                     */
+                    tnSession.AvatarData.ReturnObject = tnSession.AvatarData.SentObject.Clone();
+
                     break;
 
                 default:
                     LogEvent.Trace(2, tnSession.TraceLogs, Asm);
+
                     Outpost31.Core.Roundhouse.Parse(tnSession);
+
                     break;
             }
 
@@ -96,3 +113,12 @@ namespace Tingen_development
         }
     }
 }
+
+/*
+
+Development notes
+-----------------
+
+- Is there a efficient way to automatically update the service status files when Tingen is re-enabled?
+
+*/
